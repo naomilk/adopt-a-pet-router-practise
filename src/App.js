a1521b543caca96d8b103fb784c14780eb9db202
Naomi Kangwa <naomi@yawt.digital>
2022-05-09 14:30:54 +0100

    adding useHistory functionality to go back button

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
     <Router>
       <Navigation />
       <Switch>
+        <Route path="/pet-details-not-found">
+          <PetDetailsNotFound />
+        </Route>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
         <Route path="/:type/:id">
           <PetDetailsPage />
         </Route>
